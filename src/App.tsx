bbbe68cf708dec72761a1597d13d8b3c6a0de273
jiangnan <yeah_20@163.com>
2020-12-08 22:09:52 +0800

    添加路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+border: 1px solid red;
+`;
 
 function App() {
   return (
-    <div>hello</div>
+    <Router>
+      <Wrapper>
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="money"/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return <h2>页面不存在</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
 }
 
 export default App;
