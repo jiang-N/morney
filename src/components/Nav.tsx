74652e5a9c44c6ec2d75cd2736bdb8d9f1bb3450
jiang-N <yeah_20@163.com>
2020-12-11 16:06:06 +0800

    Money.tsx 之 数字面板 UI 完成

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,26 +8,26 @@ const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > ul{
-  display:flex;
+    display:flex;
     > li{
       width: 33.3333%;
       text-align: center;
-        > a{
+      > a{
         display:flex;
         flex-direction: column;
         padding: 4px 0;
         align-items:center;
+        .icon{
+          width: 24px;
+          height: 24px;
+        }
+        &.selected{
+          color: red;
           .icon{
-            width: 24px;
-            height: 24px;
-          }
-          &.selected{
-            color: red;
-            .icon{
-              fill:red;
-            }
+            fill:red;
           }
         }
+      }
       }
   }
 `;
